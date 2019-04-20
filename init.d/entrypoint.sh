111ade5dd7fdd6829f179f1bbbc9feee03d32bbf
netPI <netpi@hilscher.com>
2019-04-20 08:23:37 +0200

    add routing/gateway setting

--- init.d/entrypoint.sh
+++ init.d/entrypoint.sh
@@ -20,24 +20,49 @@ echo "starting ssh ..."
 # create netx "cifx0" ethernet network interface 
 /opt/cifx/cifx0daemon
 
-# create the corresponding Ethernet configuration file 
+# ip address configured as environment variable?
 if [ -z "$IP_ADDRESS" ]
-then 
-   ip addr add 192.168.253.1/255.255.255.0 broadcast 192.168.253.255 dev cifx0
-   ip link set cifx0 up
-else 
-
-   if [ "$IP_ADDRESS" == "dhcp" ]
-   then
-     dhclient cifx0
-   else
-     IFS=. read -r i1 i2 i3 i4 <<< "$IP_ADDRESS"
-     IFS=. read -r m1 m2 m3 m4 <<< "$SUBNET_MASK"
-     BROADCAST=$((i1 & m1 | 255-m1)).$((i2 & m2 | 255-m2)).$((i3 & m3 | 255-m3)).$((i4 & m4 | 255-m4))
-     ip addr add $IP_ADDRESS/$SUBNET_MASK broadcast $BROADCAST dev cifx0
-     ip link set cifx0 up
-   fi
+then
+  # set alternative
+  IP_ADDRESS="192.168.253.1"
 fi
+
+# subnet mask configured as environment variable?
+if [ -z "$SUBNET_MASK" ]
+then
+  # set alternative
+  SUBNET_MASK="255.255.255.0"
+fi
+
+if [ "$IP_ADDRESS" == "dhcp" ]
+then
+  # set dhcp mode
+  dhclient cifx0
+  echo "cifx0 configured to dhcp"
+else
+  #split given parameters in factors
+  IFS=. read -r i1 i2 i3 i4 <<< "$IP_ADDRESS"
+  IFS=. read -r m1 m2 m3 m4 <<< "$SUBNET_MASK"
+
+  #calculate the broadcast address
+  BROADCAST=$((i1 & m1 | 255-m1)).$((i2 & m2 | 255-m2)).$((i3 & m3 | 255-m3)).$((i4 & m4 | 255-m4))
+
+  # set ip address and subnet mask
+  ip addr add $IP_ADDRESS/$SUBNET_MASK broadcast $BROADCAST dev cifx0
+
+  echo "cifx0 ip address/subnet mask set to" $IP_ADDRESS"/"$SUBNET_MASK
+  
+  #is a getway set?
+  if [ -n $GATEWAY ]
+  then
+    NETWORK=$((i1 & m1)).$((i2 & m2)).$((i3 & m3)).$((i4 & m4))
+    ip route add $GATEWAY dev cifx0
+    ip route add $NETWORK/$SUBNET_MASK via $GATEWAY dev cifx0
+  fi
+  
+  ip link set cifx0 up
+fi
+
 
 # wait forever not to exit the container
 while true
